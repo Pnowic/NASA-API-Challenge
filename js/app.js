899028cfa30938e9d8e985c3ecb6b10e8f4cd6ea
Pnowic <nowickipiotrus@gmail.com>
2018-04-30 18:52:44 +0200

    add APOD content using AJAX

--- js/app.js
+++ js/app.js
@@ -1,2 +1,39 @@
+$(function() {
 
-var apiKey = 'YHKB4NTEwjCuLzyD68m3xDsYsM35V1VVL66yAea1';
+var apodUrl =  'https://api.nasa.gov/planetary/apod?',
+    marsUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&',
+    apiKey = 'api_key=YHKB4NTEwjCuLzyD68m3xDsYsM35V1VVL66yAea1',
+    title = $('.title'),
+    date = $('.date'),
+    image = $('.image');
+
+    //generate present date
+    var today = new Date().toISOString().substring(0, 10);
+    console.log(today);
+
+    function loadAPOD() {
+        $.ajax({
+            url: apodUrl + apiKey,
+            dataType : 'json'
+        }).done(function(response){
+            console.log(response);
+            showAPODdata(response);
+        }).fail(function(error) {
+            console.log(error);
+        })
+    }
+
+    function showAPODdata(data) {
+        title.text(data.title);
+        date.text(data.date);
+        image.css("background-image", 'url("' + data.url + '")')
+
+    }
+
+    loadAPOD()
+
+
+
+
+
+});
